3b90daa088a2d99a1e1dd81ceb264a125f9e0a3f
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-05 00:33:25 +0200

    style(ui): add color theme in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				accent: {
+					100: '#6CF3A4', //green
+					200: '#00FFD1', //blue
+					300: '#FF002E', //red
+					400: '#FFE366', //yellow
+				},
+				neutral: '#FDFDFC', //white
+				base: {
+					900: '#030712', //black
+					800: '#2D2E32', //grey
+					700: '#696971', //lightgray
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
