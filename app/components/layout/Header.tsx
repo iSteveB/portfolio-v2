df86575fabce9fc57a528b9a80676baa2c1ab7ab
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-05 18:24:37 +0200

    refacto(ui): Create a button that can be reused.

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Logo from '../ui/Logo';
 import Button from '../ui/Button';
+import MailIcon from '../icon/MailIcon';
 
 const Header = () => {
 	return (
 		<header className='flex w-full justify-between bg-base-900 px-6 py-4'>
 			<Logo />
-			<Button />
+			<Button text='Copier mon mail' icon={<MailIcon />} direction='right'/>
 		</header>
 	);
 };
--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -2,19 +2,34 @@
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
-import MailIcon from '../icon/MailIcon';
+type ButtonProps = {
+	text: string;
+	icon: JSX.Element;
+	direction?: 'left' | 'right';
+};
 
-const Button = () => {
+const Button = ({ text, icon, direction = 'left' }: ButtonProps) => {
+	const animationDirection = {
+		left: {
+			icon: 'absolute left-0 rounded-full bg-accent-100 p-3',
+			text: 'overflow-hidden whitespace-nowrap pl-10 text-neutral group-hover:overflow-visible',
+			parent: 'group relative flex size-12 items-center justify-around overflow-hidden rounded-3xl border-e-2 border-accent-100 pl-2 transition-all duration-300 ease-in-out hover:w-48'
+		},
+		right: {
+			icon: 'absolute right-0 rounded-full bg-accent-100 p-3',
+			text: 'overflow-hidden whitespace-nowrap pr-10 text-neutral group-hover:overflow-visible',
+			parent: 'group relative flex size-12 items-center justify-around overflow-hidden rounded-3xl border-s-2 border-accent-100 pl-2 transition-all duration-300 ease-in-out hover:w-48'
+		},
+	};
+
 	return (
-		<motion.div className='group relative flex size-12 items-center justify-around overflow-hidden rounded-3xl border-s-2 border-accent-100 pl-2 transition-all duration-300 ease-in-out hover:w-48'>
-			<span className='overflow-hidden whitespace-nowrap pr-10 text-neutral group-hover:overflow-visible'>
-				Copier mon mail
-			</span>
+		<motion.div className={animationDirection[direction].parent}>
+			<span className={animationDirection[direction].text}>{text}</span>
 			<Link
 				href='#'
-				className='absolute right-0 rounded-full bg-accent-100 p-3'
+				className={animationDirection[direction].icon}
 				aria-label=''>
-				<MailIcon />
+				{icon}
 			</Link>
 		</motion.div>
 	);
