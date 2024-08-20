2d05a121f83433f7a6aad002f5b67067ec1cd218
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-08-20 12:05:03 +0200

    feat: format presentation text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Section from './components/ui/Section';
 import Card from './components/ui/Card';
 import Tag from './components/ui/Tag';
 import Particles from './components/ui/Particles';
+import Link from 'next/link';
 const petrona = Petrona({ subsets: ['latin'], weight: '400' });
 
 export default function Home() {
@@ -61,35 +62,77 @@ export default function Home() {
 							text='Améliorer la visibilité en ligne en utilisant des techniques de référencement naturel.'
 						/>
 					</div>
-					<div className='w-1/2'>
+					<div className='w-1/2 text-neutral'>
 						<h2 className='text-4xl text-neutral'>À propos</h2>
-						<q className=' rounded-md bg-accent-100 px-2 font-semibold'>
-							Toujours coder comme si la personne qui maintiendra
-							votre code est un psychopathe violent qui sait où
-							vous habitez.
-						</q>
-						<span> - John Woods</span>
-						<p>
-							Moi c’est Steve, développeur web depuis 4 ans.
-							J&aspos;ai troqué ma carrière de cuistot contre
-							celle de dev, passant de la création de plats à la
-							création d&aspos;apps web! Un sacré changement, mais
-							la créativité et la rigueur sont toujours au menu !
-							Ce que j&aspos;aime dans le développement web ? On a
-							jamais fini d’apprendre et chaque jour est
-							différent, avec son lot de mauvaises surprises mais
-							aussi de bonnes (oui, oui! ). Déboggage, conception
-							de nouvelles fonctionnalités, test... Pas le temps
-							de s&aspos;ennuyer ! J’adore transformer des idées
-							en code et pour ça j’utilise React/Next.js et
-							Node.js/Nest.js ⚡.Mon objectif ? Continuer à
-							progresser, jour après jour. Comme le bon vin, je
-							compte bien m&aspos;améliorer avec le temps (sans
-							les maux de tête).Si vous cherchez un développeur
-							passionné, créatif et toujours partant pour relever
-							de nouveaux défis, contactez-moi en cliquant juste
-							ici !
-						</p>
+						<mark className='rounded-sm bg-accent-100 px-2 leading-7'>
+							<q className='font-semibold'>
+								Toujours coder comme si la personne qui
+								maintiendra votre code est un psychopathe
+								violent qui sait où vous habitez.
+							</q>
+							<span> - John Woods</span>
+						</mark>
+						<div className='mt-3 flex flex-col gap-4'>
+							<p>
+								Moi c’est Steve,{' '}
+								<strong className='text-accent-100'>
+									développeur web
+								</strong>{' '}
+								depuis 4 ans.
+							</p>
+							<p>
+								J&apos;ai troqué ma carrière de cuistot contre
+								celle de dev, passant de la création de plats à
+								la création d&apos;apps web ! Un sacré
+								changement, mais la{' '}
+								<strong className='text-accent-100'>
+									créativité
+								</strong>{' '}
+								et la{' '}
+								<strong className='text-accent-100'>
+									rigueur
+								</strong>{' '}
+								sont toujours au menu !
+							</p>
+							<p>
+								<span className='text-accent-100'>
+									Ce que j&apos;aime dans le développement web
+									?{' '}
+								</span>
+								On a jamais fini d’apprendre et chaque jour est
+								différent, avec son lot de mauvaises surprises
+								mais aussi de bonnes (oui, oui! ). Déboggage,
+								conception de nouvelles fonctionnalités, test...
+								Pas le temps de s&apos;ennuyer ! J’adore
+								transformer des idées en code et pour ça
+								j’utilise{' '}
+								<strong className='text-accent-100'>
+									React/Next.js
+								</strong>{' '}
+								et{' '}
+								<strong className='text-accent-100'>
+									Node.js/Nest.js
+								</strong>
+								⚡.
+							</p>
+							<p>
+								<span className='text-accent-100'>
+									Mon objectif ?
+								</span>{' '}
+								Continuer à progresser, jour après jour. Comme
+								le bon vin, je compte bien m&apos;améliorer avec
+								le temps (sans les maux de tête).Si vous
+								cherchez un développeur passionné, créatif et
+								toujours partant pour relever de nouveaux défis,
+								contactez-moi en cliquant juste{' '}
+								<Link
+									href='/contact'
+									className='text-accent-100 underline'>
+									ici
+								</Link>{' '}
+								!
+							</p>
+						</div>
 					</div>
 				</div>
 			</Section>
