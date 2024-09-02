0ff21a3d7093e2e835d4ac1c5cd2c7b293b2dfac
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-09-02 11:37:50 +0200

    feat: responsive header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,44 +1,87 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import Logo from '../ui/Logo';
 import Button from '../ui/Button';
 import MailIcon from '../icon/MailIcon';
 import CVIcon from '../icon/CVIcon';
 import LinkedInIcon from '../icon/LinkedInIcon';
 import GitHubIcon from '../icon/GitHubIcon';
-
+import useWindowSize from '@/app/hook/useWindowSize';
 
 const Header = () => {
-	return (
-		<header className='flex w-full max-w-screen-2xl justify-between bg-base-900 px-6 py-4'>
-			<Logo />
-			<div className='flex gap-4'>
-				<Button
-					text='Mon e-mail'
-					icon={<MailIcon />}
-					direction='right'
-					email='contact@stevebasse.fr'
-				/>
-				<Button
-					text='LinkedIn'
-					icon={<LinkedInIcon />}
-					direction='right'
-					link='https://www.linkedin.com/in/steve-basse/'
-				/>
-				<Button
-					text='GitHub'
-					icon={<GitHubIcon />}
-					direction='right'
-					link='https://github.com/isteveb'
-				/>
-				<Button
-					text='CV'
-					icon={<CVIcon />}
-					direction='right'
-					link='/download/cv-steve-basse.pdf'
-				/>
-			</div>
-		</header>
-	);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+	const { width } = useWindowSize();
+
+  useEffect(() => {
+		setIsMobile(width !== undefined && width <= 768);
+	}, [width]);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const buttons = [
+    { text: 'Mon e-mail', icon: <MailIcon />, email: 'contact@stevebasse.fr' },
+    { text: 'LinkedIn', icon: <LinkedInIcon />, link: 'https://www.linkedin.com/in/steve-basse/' },
+    { text: 'GitHub', icon: <GitHubIcon />, link: 'https://github.com/isteveb' },
+    { text: 'CV', icon: <CVIcon />, link: '/download/cv-steve-basse.pdf' },
+  ];
+
+  return (
+    <header className='flex w-full max-w-screen-2xl justify-between bg-base-900 px-6 py-4'>
+      <Logo />
+      {isMobile ? (
+        <div className='relative'>
+          <button
+            onClick={toggleMenu}
+            className='text-white focus:outline-none'
+            aria-label='Menu'
+          >
+            <svg
+              className='size-8 text-accent-100'
+              fill='none'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path d='M4 6h16M4 12h16m-7 6h7'></path>
+            </svg>
+          </button>
+          {isMenuOpen && (
+            <div className='absolute right-0 z-10 mt-2 w-auto rounded-md bg-base-800 py-1 shadow-lg'>
+              {buttons.map((button, index) => (
+                <div key={index} className='px-4 py-2'>
+                  <Button
+                    text={button.text}
+                    icon={button.icon}
+                    direction='right'
+                    email={button.email}
+                    link={button.link}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className='flex gap-4'>
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              text={button.text}
+              icon={button.icon}
+              direction='right'
+              email={button.email}
+              link={button.link}
+            />
+          ))}
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
