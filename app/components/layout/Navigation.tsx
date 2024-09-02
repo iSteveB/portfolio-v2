4b1e087788022728584053d5ae8f90d5984495e7
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-09-02 11:58:09 +0200

    feat: responsive navbar

--- app/components/layout/Navigation.tsx
+++ app/components/layout/Navigation.tsx
@@ -9,14 +9,33 @@ import UserIcon from '../icon/UserIcon';
 import WorkIcon from '../icon/WorkIcon';
 
 const Navigation = () => {
-  return (
-    <motion.nav className='sticky top-1/2 z-50 ml-6 flex -translate-y-1/2 flex-col justify-center gap-3 rounded-lg bg-base-800 px-4 py-6'>
-      <Button text='Accueil' link='#hero' icon={<HomeIcon />} isInternalLink={true} />
-      <Button text='À propos' link='#about' icon={<UserIcon />} isInternalLink={true} />
-      <Button text='Portfolio' link='#portfolio' icon={<WorkIcon />} isInternalLink={true} />
-      <Button text='Mon e-mail' icon={<MailIcon />} email='contact@stevebasse.fr' />
-    </motion.nav>
-  );
+	return (
+		<motion.nav className='fixed -bottom-1 z-50 flex w-screen justify-around gap-3 bg-base-800 px-4 py-6 lg:sticky lg:top-1/2 lg:ml-6 lg:w-auto lg:-translate-y-1/2 lg:flex-col lg:justify-center lg:rounded-lg'>
+			<Button
+				text='Accueil'
+				link='#hero'
+				icon={<HomeIcon />}
+				isInternalLink={true}
+			/>
+			<Button
+				text='À propos'
+				link='#about'
+				icon={<UserIcon />}
+				isInternalLink={true}
+			/>
+			<Button
+				text='Portfolio'
+				link='#portfolio'
+				icon={<WorkIcon />}
+				isInternalLink={true}
+			/>
+			<Button
+				text='Mon e-mail'
+				icon={<MailIcon />}
+				email='contact@stevebasse.fr'
+			/>
+		</motion.nav>
+	);
 };
 
 export default Navigation;
